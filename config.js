df66d33f91bc1c80be1373ad31fed6776b9051d3
Dilial <dariopiedrasgonzalez@gmail.com>
2025-02-12 20:52:00 +0100

    Fix typo in config and register slash commands on bot ready

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = {
     guildId: process.env.GUILD_ID,
     channelId: process.env.verificationChannel,
     roleId: process.env.verificationRole,
-    predix: process.env.prefix,
+    prefix: process.env.prefix,
 }
